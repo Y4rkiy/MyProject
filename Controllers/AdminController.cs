1295d4ebf41c578f10a873df056da8efeac3e101
Y4rkiy <yaroslav.borodin@nure.ua>
2021-09-26 20:01:47 +0300

    added user controller and view

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyProject.Data;
+
+namespace MyProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        readonly ApplicationDbContext _context;
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
